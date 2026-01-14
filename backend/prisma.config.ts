07e9edc8982b7e0a8c413ff27d1ed8cfb27127e6
Sharmeen <sharmeenk2020@gmail.com>
2026-01-14 23:48:15 +0530

    prsima orm schema

---
+++ backend/prisma.config.ts
@@ -0,0 +1,11 @@
+// import { defineConfig } from '@prisma/config';
+
+// export default defineConfig({
+//   schema: {
+//     kind: 'local',
+//     path: './prisma/schema.prisma',
+//   },
+//   migrate: {
+//     url: process.env.DATABASE_URL,
+//   },
+// });
